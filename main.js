f41db2a2fafe96fb688b80266682ec913bd9b27e
Yosuke ONOUE <onoue@likr-lab.com>
2017-06-12 13:23:40 +0900

    update for 2017

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-var getCSV = d3.dsv(',', 'text/csv; charset=shift_jis');
-getCSV('data.csv', (data) => {
-  // TODO ここに処理を書く
-});
+/* global d3 */
+d3.request('data.csv')
+  .mimeType('text/csv; charset=shift_jis')
+  .response((xhr) => d3.csvParse(xhr.responseText))
+  .get((data) => {
+    // TODO ここに処理を書く
+  })
